77a7a4c8fbc0bc695f4089db41eef0606304038d
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-01-18 18:01:58 +0100

    Added new level (Shifty Showdown - Stealay)
    Not playable via menu yet, only through scene itself
    
    Co-Authored-By: thomasstu <202801401+thomasstu@users.noreply.github.com>

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -5,17 +5,20 @@ extends Node
 @export var levelScene:PackedScene
 @export var level2Scene:PackedScene
 @export var level3Scene:PackedScene
+@export var level4Scene:PackedScene
 
 enum LevelIds {
 	LEVEL1,
 	LEVEL2,
-	LEVEL3
+	LEVEL3,
+	LEVEL4
 }
 
 @onready var level_dict: Dictionary[LevelIds, PackedScene] = {
 	LevelIds.LEVEL1: levelScene,
 	LevelIds.LEVEL2: level2Scene,
 	LevelIds.LEVEL3: level3Scene,
+	LevelIds.LEVEL4: level4Scene,
 }
 
 func get_level_from_id(id: LevelIds) -> PackedScene:
